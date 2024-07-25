7cdf9511794dc71461b702a941f8fb5088f43b1f
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-07-24 21:10:39 -0400

    chapter 2: challenge

--- 02-publishers-and-subscribers/projects/challenge/Starter.playground/Contents.swift
+++ 02-publishers-and-subscribers/projects/challenge/Starter.playground/Contents.swift
@@ -10,21 +10,34 @@ example(of: "Create a Blackjack card dealer") {
     var deck = cards
     var cardsRemaining = 52
     var hand = Hand()
-    
+
     for _ in 0 ..< cardCount {
       let randomIndex = Int.random(in: 0 ..< cardsRemaining)
       hand.append(deck[randomIndex])
       deck.remove(at: randomIndex)
       cardsRemaining -= 1
     }
-    
+
     // Add code to update dealtHand here
-    
+    if hand.points > 21 {
+      dealtHand.send(completion: .failure(HandError.busted))
+    } else {
+      dealtHand.send(hand)
+    }
   }
   
   // Add subscription to dealtHand here
-  
-  
+  dealtHand
+    .sink { completion in
+      if case let .failure(error) = completion {
+        print(error)
+      }
+    } receiveValue: { hand in
+      print("Card: \(hand.cardString), points: \(hand.points)")
+    }
+    .store(in: &subscriptions)
+
+
   deal(3)
 }
 
