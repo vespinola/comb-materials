1cca7d647c159a3106ee7f6d5f7bfcf4fd50cbb0
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-07-25 17:16:32 -0400

    chapter 5: combining operators

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -3,7 +3,206 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "prepend(Output...)") {
+  let publisher = [3,4].publisher
+
+  publisher
+    .prepend(1,2)
+    .prepend(-1, 0)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Sequence)") {
+  let publisher = [5,6,7].publisher
+
+  publisher
+    .prepend([3,4])
+    .prepend(Set(1...3))
+    .prepend(stride(from: 6, to: 11, by: 2))
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher)") {
+  let publisher1 = [3, 4].publisher
+  let publisher2 = [1, 2].publisher
+
+  publisher1
+    .prepend(publisher2)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher) #2") {
+  let publisher1 = [3, 4].publisher
+  let publisher2 = PassthroughSubject<Int,Never>()
+
+  publisher1
+    .prepend(publisher2)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+  publisher2.send(1)
+  publisher2.send(2)
+  publisher2.send(completion: .finished)
+}
+
+example(of: "switchToLatest") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<Int, Never>()
+  let publisher3 = PassthroughSubject<Int, Never>()
+
+  let publishers = PassthroughSubject<PassthroughSubject<Int,Never>, Never>()
+
+  publishers
+    .switchToLatest()
+    .sink(
+      receiveCompletion: { _ in print("Completed!") },
+      receiveValue: { print($0) }
+    )
+    .store(in: &subscriptions)
+
+  publishers.send(publisher1)
+  publisher1.send(1)
+  publisher1.send(2)
+
+  publishers.send(publisher2)
+  publisher1.send(3)
+  publisher2.send(4)
+  publisher2.send(5)
+
+  // 6
+  publishers.send(publisher3)
+  publisher2.send(6)
+  publisher3.send(7)
+  publisher3.send(8)
+  publisher3.send(9)
+
+  publisher3.send(completion: .finished)
+  publishers.send(completion: .finished)
+}
+
+example(of: "switchToLatest - Network Request") {
+  let url = URL(string: "https://source.unsplash.com/collection/\(UUID().uuidString)")!
+
+  // 1
+  func getImage() -> AnyPublisher<UIImage?, Never> {
+    URLSession.shared
+      .dataTaskPublisher(for: url)
+      .map { data, _ in UIImage(data: data) }
+      .print("image")
+      .replaceError(with: nil)
+      .eraseToAnyPublisher()
+  }
+
+  // 2
+  let taps = PassthroughSubject<Void, Never>()
+
+  taps
+    .map { _ in getImage() } // 3
+    .switchToLatest() // 4
+    .sink(receiveValue: { _ in })
+    .store(in: &subscriptions)
+
+  // 5
+  taps.send()
+
+  DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    taps.send()
+  }
+
+  DispatchQueue.main.asyncAfter(deadline: .now() + 3.1) {
+    taps.send()
+  }
+}
+
+example(of: "merge(with:)") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<Int, Never>()
+
+  publisher1
+    .merge(with: publisher2)
+    .sink(receiveCompletion: {
+      print("Completed", $0)
+    }, receiveValue: {
+      print($0)
+    })
+    .store(in: &subscriptions)
+
+  publisher1.send(1)
+  publisher1.send(2)
+
+  publisher2.send(3)
+
+  publisher1.send(4)
+  publisher2.send(5)
+
+
+  publisher1.send(completion: .finished)
+  publisher2.send(completion: .finished)
+}
+
+example(of: "combineLatest") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<String, Never>()
+
+  // 2
+  publisher1
+    .combineLatest(publisher2)
+    .sink(
+      receiveCompletion: { _ in print("Completed") },
+      receiveValue: { print("P1: \($0), P2: \($1)") }
+    )
+    .store(in: &subscriptions)
+
+  // 3
+  publisher1.send(1)
+  publisher1.send(2)
+
+  publisher2.send("a")
+  publisher2.send("b")
+
+  publisher1.send(3)
+
+  publisher2.send("c")
+
+  // 4
+  publisher1.send(completion: .finished)
+  publisher2.send(completion: .finished)
+}
+
+example(of: "zip") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<String, Never>()
+
+  // 2
+  publisher1
+    .zip(publisher2)
+    .sink(
+      receiveCompletion: { _ in print("Completed") },
+      receiveValue: { print("P1: \($0), P2: \($1)") }
+    )
+    .store(in: &subscriptions)
+
+  // 3
+  publisher1.send(1)
+  publisher1.send(2)
+  publisher2.send("a")
+  publisher2.send("b")
+  publisher1.send(3)
+  publisher2.send("c")
+  publisher2.send("d")
+
+  // 4
+  publisher1.send(completion: .finished)
+  publisher2.send(completion: .finished)
+}
+
 
 /// Copyright (c) 2023 Kodeco Inc.
 ///
